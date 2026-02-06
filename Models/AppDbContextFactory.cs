51b4f6da04d9b07022bf48dca9a3850f6cc4d942
lirije <lirije11@hotmail.com>
2026-02-06 09:56:20 +0100

    Add EF baseline migration

---
+++ Models/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace inspecto_API.Models;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseNpgsql(
+            Environment.GetEnvironmentVariable("SUPABASE_DB_CONNECTION")
+            ?? throw new InvalidOperationException("Missing SUPABASE_DB_CONNECTION")
+        );
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
